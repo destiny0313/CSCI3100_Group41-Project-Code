48a8a7400be6e959cac98d54e547c7ca6d00284e
Destiny <49312817+destiny0313@users.noreply.github.com>
2020-03-26 15:24:22 +0800

    Upload login.php
    For login page

---
+++ functions/login/login.php
@@ -0,0 +1,100 @@
+<?php
+session_start();
+
+if(isset($_SESSION["loggedin") && $_SESSION["loggedin"] === true){
+	header("location: welcome.php");
+	exit;
+}
+
+require_once "login_config.php";
+
+$username = $password = "";
+$username_err = $password_err = ""
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+	if(empty(trim($_POST["username"]))){
+		$username_err = "Please enter username.";
+	}else{
+		$username = trim($_POST["username"]);
+	}
+
+	if(empty(trim($_POST["password"]))){
+		$password_err = "Please enter password.";
+	}else{
+		$password = trim($_POST["password"]);
+	}
+
+	if(empty($username_err) && ($password_err)){
+		$sql = "SELECT id, username, password FROM users WHERE username = ?";
+
+		if(stmt = mysqli_prepare($link, $sql)){
+			mysqli_stmt_bind_param($stmt, "s", $param_username);
+			$param_username = $username;
+
+			if(mysqli_stmt_execute($stmt)){
+				mysqli_stmt_store_result($stmt);
+
+				if(mysqli_stmt_rows($stmt) == 1){
+					myseli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+					if(mysqli_stmt_fetch($stmt)){
+						if(password_verify($password, $hashed_password)){
+							session_start();
+
+							$_SESSION["loggedin"] = true;
+							$_SESSION["id"] = $id;
+							$_SESSION["username"] = $username;
+
+							header("location: welcome.php");
+						}else{
+							$password_err = "Wrong password.";
+						}
+					}
+				}else{
+					$username_err = "Username does not exist.";
+				}
+			}else{
+				echo "Oops! Something went wrong. Please try again later.";
+			}
+	
+			myseli_stmt_close($stmt);
+		}
+	}
+
+	mysqli_close($link);
+}
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Login</h2>
+        <p>Please fill in your credentials to login.</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                <label>Username</label>
+                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+                <span class="help-block"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control">
+                <span class="help-block"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Login">
+            </div>
+            <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
+        </form>
+    </div>    
+</body>
+</html>
