514c522a9a2de408d318b7d7b17fcb9e2a8f2797
Destiny <49312817+destiny0313@users.noreply.github.com>
2020-03-27 18:26:57 +0800

    Create search.php

---
+++ functions/shopping/search.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<header>Results:</header>
+<body>
+  <table>
+    <?ph
+
+    require_once "product_config.php";
+
+    $sql = "SELECT * FROM product_list WHERE Product_Name LIKE" .$search;
+    $result = mysql_query($sql);
+  
+    if($result->num_row>0){
+
+      while($row=mysql_fetch_array($result)){
+        $image = $row[Photo];
+        $imagedata = base64_encode(file_get_contents($image));
+        echo "<tr><td>", $row[Product_ID], "</td><td>", $row[Product_Name], "</td><td>", $row[Price], </td><td>, $row[Stock], </td><td>, '<img src="data:image/jpeg;base64,'.$imagedata.'">', "</td><td>", $row[Seller_ID], "</td></tr>";
+      }
+    }else{
+      echo "No result found.";
+    }
+    echo "</table>;
+
+    mysqli_close($link);
+    ?>
+  <a href = "welcome.php">Back</a>
+  </body>
+  </html>
+
