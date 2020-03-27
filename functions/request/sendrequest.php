efeeea238d8c5caad75836be4dafe94db83ab46a
Destiny <49312817+destiny0313@users.noreply.github.com>
2020-03-27 22:34:21 +0800

    Update sendrequest.php

--- functions/request/sendrequest.php
+++ functions/request/sendrequest.php
@@ -1,4 +1,14 @@
-<?ph
+<!DOCTYPE html>
+<html>
+  <header></header>
+  <body>
+    <form action="status.php" method="post">
+      <input type="Send Request
+                   
+                   
+                   
+                   
+                   <?ph
 require_once "request_config.php";
 
 ?>
