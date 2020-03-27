bd86d81a8df7d718ee5028ed224878dea5c691c0
Destiny <49312817+destiny0313@users.noreply.github.com>
2020-03-27 22:04:17 +0800

    Create search_price.php

---
+++ functions/estimation/search_price.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+  <html>
+  <header></header>
+  <body>
+  <form action="calculation.php" method="POST">
+  Search:<input type="text" name="product">
+  <input type="submit" value="Submit">
+  </form>
+  </body>
+  </html>
