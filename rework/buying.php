d2909b9a22711a1b959b7306df961bcdc8bc3620
Destiny <49312817+destiny0313@users.noreply.github.com>
2020-04-20 01:32:27 +0800

    Update buying.php

--- rework/buying.php
+++ rework/buying.php
@@ -1,4 +1,8 @@
-<?php include('server.php') ?>
+<?php
+include('server.php');
+if (!isset($_SESSION['username'])){
+  header('location: login.php');
+} ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -15,14 +19,23 @@
   </div>
 
   <form method="post" action="buying.php">
-    <?php include('errors.php'); ?>
+    <?php include('errors.php');?>
     <div class="input-group">
       <label>Search for product:</label>
       <input type="text" name="search_product" value="<?php echo $search_product;?>">
     </div>
-    <p><button type="submit" class="btn" name="search_product_submit">Search</button></p>
+    <p><button type="submit" class="btn" name="search_product_submit">Search</button>
+    <button type="submit" class="btn" name="clear_search">Clear</button></p>
+    <p><br/></p>
+    <?php include ('search_result.php')?>
+
+      <div class="input-group">
+        <label> Purchase: (Please input product id)</label>
+        <input type="number" name="purchase_this" value="<?php echo $purchase_this;?>">
+      </div>
+      <p><button onclick="location.href='purchase.php'" type="submit" class="btn" name="purchase_this_submit">Purchase</button></p>
     <p>
-      <a href="index.php">Home Page</a>
+      <a href="index.php"><br/>Home Page</a>
     </p>
     <p>
       <a href="index.php?logout='1'">Logout</a></p></center>
