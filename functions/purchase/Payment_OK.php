f9d0713cb8b422e4573fa7340d352af3ca11dcd9
Destiny <49312817+destiny0313@users.noreply.github.com>
2020-04-20 01:48:09 +0800

    Delete Payment_OK.php

--- functions/purchase/Payment_OK.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-  </head>
-  <body>
-    echo "Payment successful!";
-    <button onclick=window.location.href = 'welcome.php';">Main Page</button>
-  </body>
-</html>
